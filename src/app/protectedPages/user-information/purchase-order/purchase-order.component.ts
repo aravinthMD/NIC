b5502ca4cac4bff0dc4c664897d9a1d1a263ade1
raja-k003 <59433464+raja-k003@users.noreply.github.com>
2020-10-30 11:00:51 +0530

    custom select

--- src/app/protectedPages/user-information/purchase-order/purchase-order.component.ts
+++ src/app/protectedPages/user-information/purchase-order/purchase-order.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit,Input, AfterViewInit,ViewChild } from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
-import { Validators, FormBuilder, FormGroup,FormControl } from "@angular/forms";
+import { Component, OnInit, Input, AfterViewInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Validators, FormBuilder, FormGroup, FormControl } from "@angular/forms";
 import { LabelsService } from '../../../services/labels.service';
-import {DatePipe} from '@angular/common';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
@@ -11,70 +11,87 @@ import {DatePipe} from '@angular/common';
   templateUrl: './purchase-order.component.html',
   styleUrls: ['./purchase-order.component.scss']
 })
-export class PurchaseOrderComponent implements OnInit,AfterViewInit {
+export class PurchaseOrderComponent implements OnInit, AfterViewInit {
 
-  @ViewChild(MatPaginator,{static : true}) paginator : MatPaginator;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
-  @Input('userObj') user : any
+  @Input('userObj') user: any
 
-  displayedColumns : string[] = ['purchaseNo','projectNo','piAmt','remarks']
+  displayedColumns: string[] = ['purchaseNo', 'projectNo', 'piAmt', 'remarks']
+  poStatus: any[] = [
+    { key: 0, value: 'Received' },
+    { key: 1, value: 'Pending' },
+    { key: 2, value: 'Approved' },
+    { key: 3, value: 'Rejected' },
+    { key: 4, value: 'On Hold' }]
+  piStatus: any[] = [
+    { key: 0, value: 'Received' },
+    { key: 1, value: 'Pending' },
+    { key: 2, value: 'Approved' },
+    { key: 3, value: 'Rejected' },
+    { key: 4, value: 'On Hold' }]
 
-  userList : any[] =   [
-    {purchaseNo : 114,projectNumber : 5345,piAmt:24250,remarks:''},
-    {purchaseNo : 197,projectNumber : 5465,piAmt:25000,remarks:''},
-    {purchaseNo : 767,projectNumber : 2344,piAmt:45000,remarks:''},
-    {purchaseNo : 678,projectNumber : 2367,piAmt:24250,remarks:''},
-    {purchaseNo : 114,projectNumber : 5654,piAmt:28000,remarks:''},
-    {purchaseNo : 114,projectNumber : 5345,piAmt:34000,remarks:''},
+  piReceivedIn: any[] = [
+    { key: 0, value: 'Full' },
+    { key: 1, value: 'Partial' }]
+
+  paymentStatus: any[] = [
+    { key: 0, value: 'Pending' },
+    { key: 1, value: 'Received' },
+    { key: 2, value: 'On Hold' }]
+    
+  userList: any[] = [
+    { purchaseNo: 114, projectNumber: 5345, piAmt: 24250, remarks: '' },
+    { purchaseNo: 197, projectNumber: 5465, piAmt: 25000, remarks: '' },
+    { purchaseNo: 767, projectNumber: 2344, piAmt: 45000, remarks: '' },
+    { purchaseNo: 678, projectNumber: 2367, piAmt: 24250, remarks: '' },
+    { purchaseNo: 114, projectNumber: 5654, piAmt: 28000, remarks: '' },
+    { purchaseNo: 114, projectNumber: 5345, piAmt: 34000, remarks: '' },
   ];
   dataSource = new MatTableDataSource<any>(this.userList);
 
   date = new FormControl();
-  PurchaseOrderForm:FormGroup;
+  PurchaseOrderForm: FormGroup;
   labels: any = {};
 
   constructor(
     private labelsService: LabelsService,
-    private DatePipe:DatePipe
-    ) { }
+    private DatePipe: DatePipe
+  ) { }
 
   ngOnInit() {
 
-    this.labelsService.getLabelsData().subscribe((values)=> {
+    this.labelsService.getLabelsData().subscribe((values) => {
       this.labels = values;
-      console.log('label',this.labels)
+      console.log('label', this.labels)
     })
     this.PurchaseOrderForm = new FormGroup({
-      purchaseON:new FormControl(null),
-      projectNo:new FormControl(null),
-      projectName:new FormControl(null),
-      userId:new FormControl(null),
-      date:new FormControl(null),
-      poAmount:new FormControl(null),
-      poStatus:new FormControl(null),
-      piStatus:new FormControl(null),
-      piReceivedIn:new FormControl(null),
-      paymentStatus:new FormControl(null),
-      remark:new FormControl(null),
-      piBillable:new FormControl(null),
-      poBillable:new FormControl(null),
-      uploadDoc:new FormControl(null),
+      purchaseON: new FormControl(null),
+      projectNo: new FormControl(null),
+      projectName: new FormControl(null),
+      userId: new FormControl(null),
+      date: new FormControl(null),
+      poAmount: new FormControl(null),
+      poStatus: new FormControl(null),
+      piStatus: new FormControl(null),
+      piReceivedIn: new FormControl(null),
+      paymentStatus: new FormControl(null),
+      remark: new FormControl(null),
+      piBillable: new FormControl(null),
+      poBillable: new FormControl(null),
+      uploadDoc: new FormControl(null),
 
     })
-    
+
   }
-  POForm(){
-    this.PurchaseOrderForm.value['date']=this.DatePipe.transform(this.PurchaseOrderForm.value['date'],'dd/MM/yyyy')
-   console.log(this.PurchaseOrderForm.value)
-   this.PurchaseOrderForm.reset();
+  POForm() {
+    this.PurchaseOrderForm.value['date'] = this.DatePipe.transform(this.PurchaseOrderForm.value['date'], 'dd/MM/yyyy')
+    console.log(this.PurchaseOrderForm.value)
+    this.PurchaseOrderForm.reset();
   }
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
 
-  }
-
-  formDateFunc(event) {
-    
   }
 
 }
--- src/app/protectedPages/user-information/tax-invoice/tax-invoice.component.ts
+++ src/app/protectedPages/user-information/tax-invoice/tax-invoice.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit,Input,ViewChild,AfterViewInit } from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
-import { Validators, FormBuilder, FormGroup,FormControl } from "@angular/forms";
+import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Validators, FormBuilder, FormGroup, FormControl } from "@angular/forms";
 import { LabelsService } from 'src/app/services/labels.service';
-import {DatePipe} from '@angular/common';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-tax-invoice',
@@ -12,56 +12,59 @@ import {DatePipe} from '@angular/common';
 })
 export class TaxInvoiceComponent implements OnInit {
 
-  @ViewChild(MatPaginator,{static : true}) paginator : MatPaginator;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
-  @Input('userObj') user : any;
+  @Input('userObj') user: any;
 
-  displayedColumns : string[] = ['InvoiceNo','projectNo','piAmt','remarks']
+  displayedColumns: string[] = ['InvoiceNo', 'projectNo', 'piAmt', 'remarks']
+  paymentStatus: any[] = [
+    { key: 0, value: 'Pending' },
+    { key: 1, value: 'Received' },
+    { key: 2, value: 'On Hold' }]
+  userList: any[] = [
 
-  userList : any[] =   [
-   
-    {invoiceNo : 1343,projectNumber : 4355,piAmt:24250,remarks:''},
-    {invoiceNo : 5464,projectNumber : 5655,piAmt:35000,remarks:''},
-    {invoiceNo : 7687,projectNumber : 3424,piAmt:23450,remarks:''}
+    { invoiceNo: 1343, projectNumber: 4355, piAmt: 24250, remarks: '' },
+    { invoiceNo: 5464, projectNumber: 5655, piAmt: 35000, remarks: '' },
+    { invoiceNo: 7687, projectNumber: 3424, piAmt: 23450, remarks: '' }
 
   ];
 
   dataSource = new MatTableDataSource<any>(this.userList);
-  taxInvoiceForm:FormGroup
-  labels: any ={};
+  taxInvoiceForm: FormGroup
+  labels: any = {};
 
 
   constructor(private labelsService: LabelsService,
-    private Datepipe:DatePipe) { }
+    private Datepipe: DatePipe) { }
 
   ngOnInit() {
-    this.labelsService.getLabelsData().subscribe((values)=> {
+    this.labelsService.getLabelsData().subscribe((values) => {
       this.labels = values;
     });
-    this.taxInvoiceForm=new FormGroup({
-      taxIN:new FormControl(null),
-      invoiceDate:new FormControl(null),
-      projectNo:new FormControl(null),
-      poNumber:new FormControl(null),
-      poDate:new FormControl(null),
-      fromDate:new FormControl(null),
-      toDate:new FormControl(null),
-      invoiceAmount:new FormControl(null),
-      remark:new FormControl(null),
-      billClaim:new FormControl(null),
-      uploadDoc:new FormControl(null),
+    this.taxInvoiceForm = new FormGroup({
+      taxIN: new FormControl(null),
+      invoiceDate: new FormControl(null),
+      projectNo: new FormControl(null),
+      poNumber: new FormControl(null),
+      poDate: new FormControl(null),
+      fromDate: new FormControl(null),
+      toDate: new FormControl(null),
+      invoiceAmount: new FormControl(null),
+      remark: new FormControl(null),
+      billClaim: new FormControl(null),
+      uploadDoc: new FormControl(null),
     })
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
 
   }
-  taxInForm(){
-    this.taxInvoiceForm.value['fromDate']=this.Datepipe.transform(this.taxInvoiceForm.value['fromDate'],'dd/MM/yyyy')
-    this.taxInvoiceForm.value['toDate']=this.Datepipe.transform(this.taxInvoiceForm.value['toDate'],'dd/MM/yyyy')
-    this.taxInvoiceForm.value['poDate']=this.Datepipe.transform(this.taxInvoiceForm.value['poDate'],'dd/MM/yyyy')
-    this.taxInvoiceForm.value['invoiceDate']=this.Datepipe.transform(this.taxInvoiceForm.value['invoiceDate'],'dd/MM/yyyy')
+  taxInForm() {
+    this.taxInvoiceForm.value['fromDate'] = this.Datepipe.transform(this.taxInvoiceForm.value['fromDate'], 'dd/MM/yyyy')
+    this.taxInvoiceForm.value['toDate'] = this.Datepipe.transform(this.taxInvoiceForm.value['toDate'], 'dd/MM/yyyy')
+    this.taxInvoiceForm.value['poDate'] = this.Datepipe.transform(this.taxInvoiceForm.value['poDate'], 'dd/MM/yyyy')
+    this.taxInvoiceForm.value['invoiceDate'] = this.Datepipe.transform(this.taxInvoiceForm.value['invoiceDate'], 'dd/MM/yyyy')
     console.log(this.taxInvoiceForm.value)
     this.taxInvoiceForm.reset()
   }
