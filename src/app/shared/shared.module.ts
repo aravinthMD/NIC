656ac76a9e70334df1e6db0f668ca0e1420ada41
arunkumar <arunkumar.ravi@appiyo.com>
2020-12-04 10:49:35 +0530

    changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -25,7 +25,6 @@ import { RemarkModalComponent } from './remark-modal/remark-modal.component';
     AccountHeaderComponent,
     RemarkModalComponent
   ],
-  entryComponents : [AuditTrailDialogComponent,
-  ]
+  entryComponents : [AuditTrailDialogComponent]
 })
 export class SharedModule { }
