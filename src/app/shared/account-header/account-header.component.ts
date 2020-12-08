118167ba23fb462533af35fd5d3689d10f55084d
arunkumar <arunkumar.ravi@appiyo.com>
2020-12-08 19:04:39 +0530

    updated code

--- src/app/shared/account-header/account-header.component.ts
+++ src/app/shared/account-header/account-header.component.ts
@@ -30,7 +30,7 @@ export class AccountHeaderComponent implements OnInit {
       //   left : '1',
       // }
       dialogConfig.width = '2000px';
-      dialogConfig.height = '700px';
+      dialogConfig.height = '680px';
       dialogConfig.maxWidth = '95vw';
       const MatDialogRef =  this.dialog.open(AuditTrailDialogComponent ,dialogConfig
        );
